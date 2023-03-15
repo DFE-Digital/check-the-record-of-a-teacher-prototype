f416c9f9d150977442c7a51995298530e9db57b2
Adam Silver <adambsilver@gmail.com>
2023-03-15 17:59:39 +0000

    More filter love

--- app/routes/teachers.js
+++ app/routes/teachers.js
@@ -28,7 +28,8 @@ module.exports = router => {
     // Filters
     let yourTeachers = _.get(req.session.data.filters, 'yourTeachers')
     let prohibitionTypes = _.get(req.session.data.filters, 'prohibitionTypes')
-    let hasFilters = _.get(prohibitionTypes, 'length') || _.get(yourTeachers, 'length')
+    let prohibitionStatuses = _.get(req.session.data.filters, 'prohibitionStatuses')
+    let hasFilters = _.get(prohibitionTypes, 'length') || _.get(prohibitionStatuses, 'length') || _.get(yourTeachers, 'length')
 
     let selectedFilters = {
       categories: []
@@ -37,30 +38,23 @@ module.exports = router => {
     if(hasFilters) {
       teachers = teachers.filter(teacher => {
         let prohibitionTypesValid = true
+        let prohibitionStatusesValid = true
         let yourTeachersValid = true
 
         if(prohibitionTypes) {
           prohibitionTypesValid = hasCommonArrayValues(prohibitionTypes, teacher.prohibitionTypes)
         }
 
+        if(prohibitionStatuses) {
+          prohibitionStatusesValid = (prohibitionStatuses.includes('Has a prohibition') && teacher.prohibitionTypes.length) || (prohibitionStatuses.includes('Does not have a prohibition') && !teacher.prohibitionTypes.length)
+        }
+
         if(yourTeachers) {
           yourTeachersValid = (yourTeachers.includes('Your teacher') && teacher.yourTeacher) || (yourTeachers.includes('Not your teacher') && !teacher.yourTeacher)
         }
 
-        return prohibitionTypesValid && yourTeachersValid
+        return prohibitionTypesValid && prohibitionStatusesValid && yourTeachersValid
       })
-
-      if(_.get(yourTeachers, 'length')) {
-        selectedFilters.categories.push({
-          heading: { text: 'Your teacher' },
-          items: yourTeachers.map(yourTeacherItem => {
-            return {
-              text: yourTeacherItem,
-              href: `/teachers/remove-yourTeacher/${yourTeacherItem}`
-            }
-          })
-        })
-      }
 
       if(_.get(prohibitionTypes, 'length')) {
         selectedFilters.categories.push({
@@ -74,7 +68,29 @@ module.exports = router => {
         })
       }
 
+      if(_.get(prohibitionStatuses, 'length')) {
+        selectedFilters.categories.push({
+          heading: { text: 'Prohibition' },
+          items: prohibitionStatuses.map(prohibitionStatus => {
+            return {
+              text: prohibitionStatus,
+              href: `/teachers/remove-prohibitionStatus/${prohibitionStatus}`
+            }
+          })
+        })
+      }
 
+      if(_.get(yourTeachers, 'length')) {
+        selectedFilters.categories.push({
+          heading: { text: 'Your teacher' },
+          items: yourTeachers.map(yourTeacherItem => {
+            return {
+              text: yourTeacherItem,
+              href: `/teachers/remove-yourTeacher/${yourTeacherItem}`
+            }
+          })
+        })
+      }
 
     }
 
@@ -96,19 +112,26 @@ module.exports = router => {
     res.redirect('/teachers')
   })
 
-  router.get('/teachers/remove-prohibitionType/:prohibitionType', (req, res) => {
-    req.session.data.filters.prohibitionTypes = removeFilter(req.params.prohibitionType, req.session.data.filters.prohibitionTypes)
-    res.redirect('/teachers')
-  })
-
   router.get('/teachers/remove-yourTeacher/:yourTeacher', (req, res) => {
     req.session.data.filters.yourTeachers = removeFilter(req.params.yourTeacher, req.session.data.filters.yourTeachers)
     res.redirect('/teachers')
   })
 
+  router.get('/teachers/remove-prohibitionStatus/:prohibitionStatus', (req, res) => {
+    req.session.data.filters.prohibitionStatuses = removeFilter(req.params.prohibitionStatus, req.session.data.filters.prohibitionStatuses)
+    res.redirect('/teachers')
+  })
+
+  router.get('/teachers/remove-prohibitionType/:prohibitionType', (req, res) => {
+    req.session.data.filters.prohibitionTypes = removeFilter(req.params.prohibitionType, req.session.data.filters.prohibitionTypes)
+    res.redirect('/teachers')
+  })
+
+
   router.get('/teachers/clear-filters', (req, res) => {
-    _.set(req.session.data.filters, 'prohibitionTypes', null)
     _.set(req.session.data.filters, 'yourTeachers', null)
+    _.set(req.session.data.filters, 'prohibitionStatuses', null)
+    _.set(req.session.data.filters, 'prohibitionTypes', null)
     res.redirect('/teachers')
   })
 
