173a43c1431da211f4dc0378a576c17a4f1d34f5
Adam Silver <adambsilver@gmail.com>
2023-05-10 08:07:00 +0100

    Add search

--- app/routes/restricted-teachers.js
+++ app/routes/restricted-teachers.js
@@ -8,8 +8,18 @@ module.exports = router => {
 
   router.get('/restricted-teachers/', (req, res) => {
     let teachers = allTeachers
+
     teachers = teachers
       .filter(teacher => teacher.hasProhibitions == 'Yes')
+
+    let teacherName = _.get(req.session.data, 'teacherName').toLowerCase()
+    if(teacherName) {
+      teachers = teachers.filter(teacher => {
+        let name = (teacher.firstName + ' ' + teacher.lastName).toLowerCase()
+        return (name).indexOf(teacherName) > -1
+      })
+    }
+
     let totalCount = teachers.length
     let pagination = PaginationHelper.getPagination(teachers, req.query.page, 25)
     teachers = PaginationHelper.getDataByPage(teachers, pagination.pageNumber, 25)
@@ -21,29 +31,34 @@ module.exports = router => {
     })
   })
 
-  router.get('/restricted-teachers/:restrictionTypeId', (req, res) => {
-    let teachers = allTeachers
-    let restrictionType = restrictionTypes.find(type => type.id == req.params.restrictionTypeId).label
+  // router.get('/restricted-teachers/:restrictionTypeId', (req, res) => {
+  //   let teachers = allTeachers
+  //   let restrictionType = restrictionTypes.find(type => type.id == req.params.restrictionTypeId).label
 
-    teachers = teachers
-      .filter(teacher => teacher.hasProhibitions == 'Yes')
-      .filter(teacher => {
-        let restrictionTypeIds = teacher.restrictions.map(restriction => restriction.type.id)
-        return restrictionTypeIds.includes(req.params.restrictionTypeId)
-      })
+  //   teachers = teachers
+  //     .filter(teacher => teacher.hasProhibitions == 'Yes')
+  //     .filter(teacher => {
+  //       let restrictionTypeIds = teacher.restrictions.map(restriction => restriction.type.id)
+  //       return restrictionTypeIds.includes(req.params.restrictionTypeId)
+  //     })
 
-    // Pagination
-    let totalCount = teachers.length
-    let pagination = PaginationHelper.getPagination(teachers, req.query.page, 25)
-    teachers = PaginationHelper.getDataByPage(teachers, pagination.pageNumber, 25)
+  //   // Pagination
+  //   let totalCount = teachers.length
+  //   let pagination = PaginationHelper.getPagination(teachers, req.query.page, 25)
+  //   teachers = PaginationHelper.getDataByPage(teachers, pagination.pageNumber, 25)
 
-    res.render('restricted-teachers/index', {
-      teachers,
-      totalCount,
-      pagination,
-      restrictionType
-    })
+  //   res.render('restricted-teachers/index', {
+  //     teachers,
+  //     totalCount,
+  //     pagination,
+  //     restrictionType
+  //   })
 
+  // })
+
+  router.get('/restricted-teachers/clear-search', (req, res) => {
+    req.session.data.teacherName = ''
+    res.redirect('/restricted-teachers')
   })
 
 }
