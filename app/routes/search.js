a6f16fbea1efeb7e908e5cd0a58446662edfc193
Adam Silver <adambsilver@gmail.com>
2023-05-19 11:25:59 +0100

    Remove your teachers for now

--- app/routes/search.js
+++ app/routes/search.js
@@ -5,12 +5,10 @@ const restrictionTypes = require('../data/restriction-types.json')
 module.exports = router => {
 
   router.get('/', (req, res) => {
-    let yourTeachers = allTeachers.filter(teacher => {
-      return teacher.organisation && teacher.organisation.name == req.session.data.user.organisation.name
-    })
+    // let yourTeachers = allTeachers.filter(teacher => {
+    //   return teacher.organisation && teacher.organisation.name == req.session.data.user.organisation.name
+    // })
 
-    res.render('index', {
-      yourTeachers
-    })
+    res.render('index')
   })
 }
